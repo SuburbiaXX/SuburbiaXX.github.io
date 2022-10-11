23f26f0a214a76ea78c000e72766ebae0613a3b5
SuburbiaXX <649257940@qq.com>
2022-10-11 21:39:13 +0800

    添加导航栏天气

---
+++ themes/butterfly/source/js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "201",//改变显示顺序的012是温度、城市、图标
+      "background": "5",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "14px 10px 10px 10px",
+      "shadow": "1",
+      "language": "zh",
+      "borderRadius": "10",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "5ff613386cba4f3cbae92dbbd0116b03"
+    }
+  }
