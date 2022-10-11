78efb9fa9f1f921faa57d0fdb06957df8e70436e
SuburbiaXX <89294889+SuburbiaXX@users.noreply.github.com>
2022-10-12 00:18:21 +0800

    首页图片随机

--- themes/butterfly/source/js/rdmbkg.js
+++ themes/butterfly/source/js/rdmbkg.js
@@ -1,22 +1,14 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-var backimg =[
-    "url(https://pic.imgdb.cn/item/632b25a016f2c2beb15c3c17.jpg)",
-    "url(https://pic1.imgdb.cn/item/634279bd16f2c2beb170f64b.jpg)",
-    "url(https://pic1.imgdb.cn/item/634279bd16f2c2beb170f654.jpg)",
-    "url(https://pic1.imgdb.cn/item/633d221716f2c2beb1849241.jpg)"
+var backimg = [
+  "url(https://pic1.imgdb.cn/item/6344c3fa16f2c2beb183ba07.jpg)",
+  "url(https://pic1.imgdb.cn/item/6344c3fa16f2c2beb183ba0f.jpg)",
+  "url(https://pic1.imgdb.cn/item/6344c3fa16f2c2beb183ba17.jpg)",
+  "url(https://pic1.imgdb.cn/item/6344c20a16f2c2beb1816455.jpg)"
 ];
-  //获取背景图片总数，生成随机数
-  var bgindex =Math.floor(Math.random() * (backimg.length));
-  //重设背景图片
-document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
-  //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
-var bannerimg =[
-    "url(https://pic.imgdb.cn/item/632b25a016f2c2beb15c3c17.jpg)",
-    "url(https://pic1.imgdb.cn/item/634279bd16f2c2beb170f64b.jpg)",
-    "url(https://pic1.imgdb.cn/item/634279bd16f2c2beb170f654.jpg)",
-    "url(https://pic1.imgdb.cn/item/633d221716f2c2beb1849241.jpg)"
-];
-  //获取banner图片总数，生成随机数
-  var bannerindex =Math.ceil(Math.random() * (bannerimg.length-1));
-  //重设banner图片
-document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
+//获取背景图片总数，生成随机数
+var bgindex = Math.floor(Math.random() * backimg.length);
+//重设背景图片
+if(document.getElementsByClassName("full_page")[0]!=null){
+  document.getElementsByClassName("full_page")[0].style.backgroundImage=backimg[bgindex];
+  document.getElementById("footer").style.backgroundImage=backimg[bgindex];
+}
