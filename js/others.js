32597163f3d27c40f3fffffedaeef445e1020255
SuburbiaXX <89294889+SuburbiaXX@users.noreply.github.com>
2022-10-17 16:37:21 +0800

    Site updated: 2022-10-17 16:37:21

--- js/others.js
+++ js/others.js
@@ -1,1 +1,1 @@
-document.getElementById("card-info-btn").style.transition="all .5s";document.getElementById("card-info-btn").textContent="关注我 ٩(•̤̀ᵕ•̤́๑)";
+document.getElementById("card-info-btn").style.transition="all .5s";document.getElementById("card-info-btn").textContent="关注我 ヾ(≧▽≦)ゝ";
